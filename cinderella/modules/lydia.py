7400c0053f2ecd2075fc2c5229d2370b5f56bc09
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-06-28 20:57:34 +0530

    Update lydia.py

--- cinderella/modules/lydia.py
+++ cinderella/modules/lydia.py
@@ -87,7 +87,7 @@ def lydia(bot: Bot, update: Update):
             bot.send_message(OWNER_ID, f"lydia error: {e} occurred in {chat_id}!")
                     
 
-__mod_name__ = "ʟʏᴅɪᴀ/ᴄʜᴀᴛʙᴏᴛ"
+__mod_name__ = "Lʏᴅɪᴀ/ᴄʜᴀᴛʙᴏᴛ"
 
 __help__ = """
 Commands
