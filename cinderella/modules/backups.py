7f33121dd01154f8ffaa09d847d44f21408beb0c
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-06-28 12:39:48 +0530

    Update backups.py

--- cinderella/modules/backups.py
+++ cinderella/modules/backups.py
@@ -268,7 +268,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "ʙᴀᴄᴋᴜᴘꜱ"
+__mod_name__ = "Bᴀᴄᴋᴜᴘꜱ"
 
 __help__ = """
 *Only for chat administrator:*
