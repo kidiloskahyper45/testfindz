f9096b02d01112b4c2320426e2a330277950a88d
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-06-28 20:03:06 +0530

    Update cust_filters.py

--- cinderella/modules/cust_filters.py
+++ cinderella/modules/cust_filters.py
@@ -308,7 +308,7 @@ doin?
  - /stopall: stop all filters
 """
 
-__mod_name__ = "ꜰɪʟᴛᴇʀ"
+__mod_name__ = "Fɪʟᴛᴇʀ"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
